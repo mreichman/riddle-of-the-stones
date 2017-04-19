25dae7fd772b822ad5c9bf6c31d790982e13d12e
Max Reichman <reichmanmax@gmail.com>
2017-04-19 16:38:35 -0700

    Add positioning system for stones and scale.

--- js/scripts.js
+++ js/scripts.js
@@ -12,34 +12,34 @@ function Scale(timesUsed, leftWeight, rightWeight) {
 
 var stones = [];
 for (var i=1; i<=6; i++) {
-  stones.push(new Stone(i, 1, "left"));
+  stones.push(new Stone(i, 1, "none"));
 }
 for (var i=1; i<=5; i++) {
-  stones.push(new Stone(i, 1, "right"));
+  stones.push(new Stone(i, 1, "none"));
 }
 stones.push(new Stone(12, 2, "none"));
-console.log(stones);
 
 var scale = new Scale(0, 0, 0);
 
 function weigh() {
   stones.forEach(function(stone) {
     if (stone.side === "left") {
-      scale.leftWeight += 1;
+      scale.leftWeight += stone.weight;
     }
     if (stone.side === "right") {
-      scale.rightWeight += 1;
+      scale.rightWeight += stone.weight;
     }
   });
   scale.timesUsed += 1;
-  console.log(scale.timesUsed);
   var balance = (scale.leftWeight - scale.rightWeight);
   return balance;
 }
 
 $(document).ready(function() {
   $(".stone").draggable();
-  weighResult = weigh();
-  console.log(weighResult);
   $("#stone1").data(stones[0]);
+  var scalePosition = $(".row").position();
+  console.log(scalePosition);
+  var positionStone1 = $("#stone1").position();
+  console.log(positionStone1);
 });
